42360341c793fd96c7ee93cf1d64ec55ad13692e
felitba <feli@itba.edu.ar>
2026-02-05 15:30:22 -0300

    feat: game logic implement

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,7 +1,9 @@
 import Header from "./Header";
-import { useState,useEffect } from "react";
 import Card from "./Card";
+
+import { useState,useEffect,useRef } from "react";
 import { shuffle } from "../utils/shuffle";
+
 type Gif = {
   id: string;
   images: {
@@ -11,29 +13,53 @@ type Gif = {
   };
 };
 
+type GiphyResponse = {
+  data: Gif[];
+};
+
+
 function CardList(){
 
     const API_KEY = "2u23y0WAD7WHXY7dvSCdehA6sy8cSRA3"
     const [gifs, setGifs] = useState<Gif[]>([]);
+    const [score, setScore] = useState<number>(0);
+    const [bestScore, setBestScore] = useState<number>(0);
+    const gifSet = useRef(new Set<string>());
 
    useEffect(() => {
         const loadGifs = async () => {
-            await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=frog-fric&limit=10`)
-            .then(res=> res.json())
-            .then(data=> setGifs(data.data));
+            const data:GiphyResponse = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=frog-fric&limit=10`)
+            .then(res=> res.json());
+            setGifs(data.data);
   };
         loadGifs();
 }, []);
 
-    const handleClick= ()=> {setGifs(shuffle(gifs))};
+    useEffect(()=>{
+        if(score> bestScore){
+            setBestScore(score);
+        }
+    },[score]);
+
+    const handleClick= (gifId:string)=> {
+        setGifs(shuffle(gifs));
+        if (!gifSet.current.has(gifId)){
+            setScore(s=> s+1);
+            gifSet.current.add(gifId);
+        } 
+            else {
+                setScore(0);
+                gifSet.current.clear();
+            }
+    };
 
     const cardItems = gifs.map(gif=>{
-        return <Card key = {gif.id} receivedGif={gif} handleClick = {handleClick}/>
+        return <Card key = {gif.id} receivedGif={gif} handleClick = {()=>handleClick(gif.id)}/>
     })
 
     return (
         <>
-        <Header/>
+        <Header score={score} bestScore={bestScore}/>
         <div>
             {cardItems}
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import "../styles/styles.css"
-function Header(){
+
+type HeaderProps={
+    score: number;
+    bestScore: number;
+}
+
+function Header({score,bestScore}:HeaderProps){
     return (
         <div className="Header">
             <div>
-                <h1> Score: </h1>
-                <h1>Best Score: </h1>
+                <h1> Score: {score}</h1>
+                <h1>Best Score: {bestScore} </h1>
             </div>
             <div className="Header-title">
             <h1>Memory Game</h1>
