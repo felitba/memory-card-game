ba21aede1d66c15e779bb1ad779fac700a951451
felitba <feli@itba.edu.ar>
2026-02-18 15:26:19 -0300

    revert

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -41,13 +41,9 @@ function CardList({ score, setScore, bestScore, setBestScore, setGameState,level
 
    useEffect(() => {
         const loadGifs = async () => {
-        const endpoint = import.meta.env.DEV
-        ? `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=frog-fric&limit=${level * 4}`
-        : `/api/gifs/search?q=frog-fric&limit=${level * 4}`;
-
-        const response = await fetch(endpoint);
-        const data: GiphyResponse = await response.json();
-        setGifs(data.data);
+            const data:GiphyResponse = await fetch(`/api/v1/gifs/search?api_key=${API_KEY}&q=frog-fric&limit=${level * 4}`)
+            .then(res=> res.json());
+            setGifs(data.data);
 
   };
         loadGifs();
